45bd1b996d37464cc1841989a687adbd9e70d36f
Emmanuel Isabirye <emmanuelisabirye@Emmanuels-MacBook-Pro-2.local>
2019-07-01 15:45:26 +0300

    Chore(Empty react project setup) - Creating a react project using webpack4 and Babel
    [Finishes #166187806]

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+        <h1> Welcome to Author's Haven</h1>
+    );
+  }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import ReactDOM from "react-dom"; 
+import App from "./App";
+import React from "react";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
