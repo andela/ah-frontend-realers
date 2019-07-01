49871ebb9a8f06882978f8a2fc9c760192ebe665
Emmanuel Isabirye <emmanuelisabirye@Emmanuels-MacBook-Pro-2.local>
2019-07-01 15:45:26 +0300

    Chore(Empty react project setup) - Creating a react project using webpack4 and Babel
    [Finishes #166187806]

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1> Welcome to Author&apos;s Haven</h1>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+};
