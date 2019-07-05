1b2a933ea5ef4ffbf0a13f86b2c99300542fc3eb
Emmanuel Isabirye <emmanuelisabirye@Emmanuels-MacBook-Pro-2.local>
2019-07-05 17:53:59 +0300

    chore(#166187807): Redux Setup
    - Created the appropriate file structure for React/Redux workflow
    
    [Maintains #166187807]

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/index';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
